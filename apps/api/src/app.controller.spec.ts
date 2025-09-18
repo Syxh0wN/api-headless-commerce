7f3860df09805be40c464850f28e86ffe922c592
Syxh0wN <leonardobennr@gmail.com>
2025-09-18 01:26:32 -0300

    fix: atualizar script do Prettier para nova estrutura de pastas

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Headless Commerce API - Funcionando!');
+      expect(appController.getHello()).toBe(
+        'Headless Commerce API - Funcionando!',
+      );
     });
   });
 });
